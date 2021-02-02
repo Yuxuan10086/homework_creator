0381f1f03a5c2a90e9f07cd82de862b857779515
tkinter-zyx <2425352210@qq.com>
2021-02-02 12:20:49 +0800

    实现了读取输入并生成文档

--- homework_creater.py
+++ homework_creater.py
@@ -7,16 +7,19 @@ def rand_select(option):
 
 def word(text):
     from docx import Document
-    from docx.shared import Pt
-    from docx.shared import Inches
     from docx.oxml.ns import qn
+    from docx.shared import RGBColor
     font = ["萌妹子体", "李国夫手写体", "陈静的字完整版"]
     doc = Document()
     para = doc.add_paragraph(" ")
-    for i in text:
-        run = para.add_run(i)
-        run.font.name = rand_select(font)
-        run.font.size = 300000
-        run._element.rPr.rFonts.set(qn('w:eastAsia'), run.font.name)
+    for line in text:
+        for i in line:
+            run = para.add_run(i)
+            run.font.name = rand_select(font)
+            run.font.size = 300000
+            run.font.color.rgb = RGBColor(60, 60, 60)
+            run._element.rPr.rFonts.set(qn('w:eastAsia'), run.font.name)
     doc.save("temporary\\text.docx")
-word("功能：读取txt文本，然后将目的字符串标红，再将处理过的字符串写入docx中txt文本内容：啊打发发烧鳌太路线点击点击诶的骄傲计划将鳌太标红代码：")
+
+with open("input.txt", "r", encoding='utf-8') as f:
+     word(f.readlines())
