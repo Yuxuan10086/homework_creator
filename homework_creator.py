58ac2fb1292c4cc5f6c727af5eda78c635977045
Yuxuan <2425352210@qq.com>
2022-01-29 23:50:07 +0800

    解决了文件路径问题

--- homework_creator.py
+++ homework_creator.py
@@ -1,3 +1,4 @@
+import os
 def rand_select(option):
     import random
     rand = random.random()
@@ -9,6 +10,7 @@ def word(text):
     from docx import Document
     from docx.oxml.ns import qn
     from docx.shared import RGBColor
+    
     font = ["萌妹子体", "李国夫手写体", "陈静的字完整版"]
     doc = Document()
     para = doc.add_paragraph(" ")
@@ -19,33 +21,33 @@ def word(text):
             run.font.size = 300000
             run.font.color.rgb = RGBColor(60, 60, 60)
             run._element.rPr.rFonts.set(qn('w:eastAsia'), run.font.name)
-    doc.save("temporary\\text.docx")
+    doc.save(os.path.split(os.path.realpath(__file__))[0] + "\\temporary\\text.docx")
 
 def word_to_jpg():
     from docx2pdf import convert
-    import os
+    
     from PyPDF2 import PdfFileReader
-    convert("temporary\\text.docx", "temporary\\text.pdf")
-    reader = PdfFileReader("temporary\\text.pdf")
+    convert(os.path.split(os.path.realpath(__file__))[0] + "\\temporary\\text.docx", os.path.split(os.path.realpath(__file__))[0] + "\\temporary\\text.pdf")
+    reader = PdfFileReader(os.path.split(os.path.realpath(__file__))[0] + "\\temporary\\text.pdf")
     if reader.isEncrypted:
         reader.decrypt('')
     page = reader.getNumPages()
     for i in range(page):
-        os.system("mutool draw -o temporary\\text" + str(i + 1) + ".png -w 1998 -h 2585 temporary\\text.pdf " + str(i + 1))
+        os.system(os.path.split(os.path.realpath(__file__))[0] + "\\mutool.exe draw -o " + os.path.split(os.path.realpath(__file__))[0] + "\\temporary\\text" + str(i + 1) + ".png -w 1998 -h 2585 " + os.path.split(os.path.realpath(__file__))[0] + "\\temporary\\text.pdf " + str(i + 1))
     return page
 
 def pho_mix(page):
     import cv2
     import copy
     import random as ra
-    background = cv2.imread('background.jpg')
+    background = cv2.imread(os.path.split(os.path.realpath(__file__))[0] + '\\background.jpg')
     offset_x = 30
     offset_y = 100
     for num in range(page):
-        word = cv2.imread('temporary\\text' + str(num + 1) + '.png')
+        word = cv2.imread(os.path.split(os.path.realpath(__file__))[0] + '\\temporary\\text' + str(num + 1) + '.png')
         word = cv2.resize(word, (0, 0), fx = 1.1, fy = 1.2)
         res = copy.deepcopy(background)
-        print('start ' + str(num + 1) + '/' + str(page + 1))
+        print('start ' + str(num + 1) + '/' + str(page))
         for i in range(len(word)):
             for j in range(len(word[0])):
                 for k in range(3):
@@ -57,14 +59,14 @@ def pho_mix(page):
         rows, cols, channel = res.shape
         M = cv2.getRotationMatrix2D((cols / 2, rows / 2), ra.random() * 8 - 4, 1.085)
         res = cv2.warpAffine(res, M, (cols, rows))
-        cv2.imwrite('res\\res' + str(num + 1) + '.jpg', res)
+        cv2.imwrite(os.path.split(os.path.realpath(__file__))[0] + '\\res\\res' + str(num + 1) + '.jpg', res)
 
 def main():
-    import os
-    path = os.path.dirname(__file__)
+    
+    path = os.path.split(os.path.realpath(__file__))[0]
     for i in os.listdir(path + '\\res'):
-        os.remove('res\\' + i)
-    with open("input.txt", "r", encoding='utf-8') as f:
+        os.remove(path + '\\res\\' + i)
+    with open(path + "\\input.txt", "r", encoding='utf-8') as f:
         word(f.readlines())
     if input("continue?(Y/N)") == "Y":
         pass
@@ -72,7 +74,7 @@ def main():
         return
     pho_mix(word_to_jpg())
     for i in os.listdir(path + '\\temporary'):
-        os.remove('temporary\\'+ i)
+        os.remove(os.path.split(os.path.realpath(__file__))[0] + '\\temporary\\'+ i)
 
 if __name__=='__main__':
     main()
